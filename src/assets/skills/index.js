1713f85633a46368e6f6eed78075b905417759c2
FLeiras <fleiras18@gmail.com>
2025-01-18 14:34:40 -0300

    fix(index.js):Added fix to index.js to skills

--- src/assets/skills/index.js
+++ src/assets/skills/index.js
@@ -1,18 +1,39 @@
-export * from './bootstrap.png';
-export * from './css.png';
-export * from './dart1.png';
-export * from './docker.png';
-export * from './express.png';
-export * from './flutter.png';
-export * from './git.png';
-export * from './html.png';
-export * from './js.png';
-export * from './mongo.png';
-export * from './node.png';
-export * from './pinia.png';
-export * from './pos.png';
-export * from './postman.png';
-export * from './react.png';
-export * from './redux.png';
-export * from './ts.png';
-export * from './vue.png';
+import bootstrap from './bootstrap.png';
+import css from './css.png';
+import dart from './dart1.png';
+import docker from './docker.png';
+import express from './express.png';
+import flutter from './flutter.png';
+import git from './git.png';
+import html from './html.png';
+import js from './js.png';
+import mongo from './mongo.png';
+import node from './node.png';
+import pinia from './pinia.png';
+import pos from './pos.png';
+import postman from './postman.png';
+import react from './react.png';
+import redux from './redux.png';
+import ts from './ts.png';
+import vue from './vue.png';
+
+export {
+  bootstrap,
+  css,
+  dart,
+  docker,
+  express,
+  flutter,
+  git,
+  html,
+  js,
+  mongo,
+  node,
+  pinia,
+  pos,
+  postman,
+  react,
+  redux,
+  ts,
+  vue,
+};
